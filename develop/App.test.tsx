4c3acca618bedf1db434f1fa095ce7a8bb6afd71
Marigza <marigola@mail.ru>
2023-08-15 13:05:02 +0300

    feat: delete unused files

--- develop/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { describe, expect, test } from "@jest/globals";
-
-import sum from "./src/testFunction";
-
-describe("sum module", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-});
