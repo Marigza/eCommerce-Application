ef850459effef51f8bbb9cbbcc566746120df126
mikhaelHan <hancharuk270185@mail.ru>
2023-08-30 16:14:06 +0300

    feat: add the getUserInfo function

--- develop/src/components/userProfil/UserProfil.tsx
+++ develop/src/components/userProfil/UserProfil.tsx
@@ -1,11 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { getUserInfo } from "../../client_Api/userInfo";
+import { IAddresses, IUser } from "./interfaces";
 
 import "./UserProfil.scss";
 
+class Address {
+  constructor(
+    public id: string,
+    public country: string,
+    public city: string,
+    public streetName: string,
+    public postalCode: string
+  ) {}
+}
+
 const UserProfil: React.FC = () => {
-  const hello = () => {
-    console.log("hello");
+  const addressGenerate = (body: IAddresses): JSX.Element => {
+    return (
+      <div className="user-profil__addres-data_info-box">
+        <h4>{body.id}</h4>
+        <p>
+          <span>Country</span>
+          {body.country}
+        </p>
+        <p>
+          <span>City</span>
+          {body.city}
+        </p>
+        <p>
+          <span>Street</span>
+          {body.streetName}
+        </p>
+        <p>
+          <span>Postal code</span>
+          {body.postalCode}
+        </p>
+        <div className="user-profil__addres-data_button-box">
+          <button>Change</button>
+          <button>remove</button>
+        </div>
+      </div>
+    );
+  };
+
+  const userInfo = async (): Promise<void> => {
+    const user: IUser | null = await getUserInfo();
+
+    if (!user) return;
+
+    const addresCollection: IAddresses[] = user.addresses.map((el: IAddresses) => {
+      const id: string = user.billingAddressIds.includes(el.id)
+        ? "Billing"
+        : user.shippingAddressIds.includes(el.id)
+        ? "Shipping"
+        : "";
+      const addres: IAddresses = new Address(id, el.country, el.city, el.streetName, el.postalCode);
+      return addres;
+    });
+
+    const addresElement = addresCollection.map((el: IAddresses) => addressGenerate(el));
   };
 
   return (
@@ -20,7 +74,7 @@ const UserProfil: React.FC = () => {
             <div className="user-profil__personal-data">
               <div className="user-profil__email-box">
                 <p>hancharuk270185@mail.ru</p>
-                <button>Change Email</button>
+                <button onClick={userInfo}>Change Email</button>
               </div>
               <div className="user-profil__change-password">
                 <button>Change Password</button>
---
+++ develop/src/components/userProfil/interfaces.ts
@@ -0,0 +1,33 @@
+export interface IAddresses {
+  id: string;
+  country: string;
+  streetName: string;
+  postalCode: string;
+  city: string;
+}
+
+export interface IUser {
+  addresses: IAddresses[];
+  authenticationMode: string;
+  billingAddressIds: string[];
+  createdAt: string;
+  createdBy: { clientId: string; isPlatformClient: boolean };
+  dateOfBirth: string;
+  email: string;
+  firstName: string;
+  id: string;
+  isEmailVerified: boolean;
+  lastMessageSequenceNumber: number;
+  lastModifiedAt: string;
+  lastModifiedBy: { clientId: string; isPlatformClient: boolean };
+  lastName: string;
+  password: string;
+  shippingAddressIds: string[];
+  stores: { key: string; typeId: string }[];
+  version: number;
+  versionModifiedAt: string;
+  key?: string;
+  customerNumber?: string;
+  defaultShippingAddressId?: string;
+  defaultBillingAddressId?: string;
+}
