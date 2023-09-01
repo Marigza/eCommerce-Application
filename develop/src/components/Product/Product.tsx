1bf1b1964b610b8d06dc3f3b280852a3ac2cee41
Marigza <marigola@mail.ru>
2023-09-01 21:20:34 +0300

    feat: fill main page by products

--- develop/src/components/Product/Product.tsx
+++ develop/src/components/Product/Product.tsx
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom";
+import { IProduct } from "../../client_Api/interfaces";
+
 import "./Product.scss";
 
-const Product: React.FC = () => {
+type ProductType = {
+  product: IProduct;
+};
+
+const Product: React.FC<ProductType> = (props) => {
   return (
     <Link to="/product" className="product">
-      <h3>Item</h3>
-      <div>Image</div>
-      <div>description</div>
-      <button>more info...</button>
+      <h3>{props.product.masterData.staged.name["en-US"]}</h3>
+      <div>
+        <img
+          className="imageItem"
+          src={props.product.masterData.staged.masterVariant.images[0].url}
+          alt="foto"
+          height="200"
+        />
+      </div>
+      <div>
+        {props.product.masterData.staged.masterVariant.prices[0].value.centAmount / 100}
+        {props.product.masterData.staged.masterVariant.prices[0].value.currencyCode}
+      </div>
+      <button className="button__more_info">more info...</button>
     </Link>
   );
 };
