1b5efbea2a04788d2c6225f6029b3ceb5fa3a5cd
Marigza <marigola@mail.ru>
2023-09-02 13:59:10 +0300

    refactor: split call functional and render functional

---
+++ develop/src/client_Api/useAllProducts.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { tokenGenerate } from "./tokenGenerate";
+import { IProduct } from "./interfaces";
+
+export function useAllProducts() {
+  const [Products, setProducts] = useState<IProduct[]>([]);
+  const projectKey = "just-develop23";
+
+  const getProducts = async () => {
+    const objectToken: string | null = await tokenGenerate();
+    if (objectToken) {
+      const response = await fetch(
+        `https://api.europe-west1.gcp.commercetools.com/${projectKey}/products`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${objectToken}`,
+          },
+        }
+      );
+      const products = await response.json();
+      setProducts(products.results);
+      return products.results;
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  });
+
+  return { Products };
+}
