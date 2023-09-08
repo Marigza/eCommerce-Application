6922ee7b7ea032d42891b4d724fd285d235ba81c
Marigza <marigola@mail.ru>
2023-09-08 17:03:31 +0300

    feat: add styles to empty basket

--- develop/src/components/BasketContent/BasketContent.tsx
+++ develop/src/components/BasketContent/BasketContent.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
+import "./BasketContent.scss";
+
 const BasketContent: React.FC = () => {
   return (
     <main className="BasketContent">
-      <div className="basker-content__block">here is basket</div>
+      <h2 className="basket_header">Basket</h2>
+      {/* <div className="items_block"></div> */}
+      {/* when a cart isn't empty delete next block*/}
+      <div className="empty_block">
+        <div className="empty_block-message">
+          <div className="empty_block-text">Oops! Your cart is empty! You can</div>
+          <Link to="/catalog" className="cart__link">
+            <div>start shopping</div>
+          </Link>
+          <div className="empty_block-text">just now. Welcome!</div>
+        </div>
+      </div>
     </main>
   );
 };
