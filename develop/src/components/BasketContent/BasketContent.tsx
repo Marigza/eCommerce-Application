a299fba921c78682ec3ebf89115ab4a34339fcdc
Marigza <marigola@mail.ru>
2023-09-12 23:51:39 +0300

    feat: add layout for FillBasket

--- develop/src/components/BasketContent/BasketContent.tsx
+++ develop/src/components/BasketContent/BasketContent.tsx
@@ -1,15 +1,29 @@
+import { useState, useEffect } from "react";
 import { EmptyBasket } from "./EmptyBasket";
 import { FillBasket } from "./FillBasket";
+import { ICart } from "../../client_Api/interfaces";
 
 import "./BasketContent.scss";
+import { getCart } from "../../client_Api/carts";
 
 const BasketContent: React.FC = () => {
+  const [Cart, setCart] = useState<ICart>();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await getCart();
+      if (!response) return null;
+      setCart(response);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <main className="BasketContent">
       <h2 className="basket_header">Basket</h2>
-      {/* <FillBasket /> */}
-      {/* when a cart isn't empty delete next block*/}
-      <EmptyBasket />
+      {Cart?.lineItems.length && <FillBasket cart={Cart} />}
+      {!Cart?.lineItems.length && <EmptyBasket />}
     </main>
   );
 };
--- develop/src/components/BasketContent/FillBasket/FillBasket.tsx
+++ develop/src/components/BasketContent/FillBasket/FillBasket.tsx
@@ -1,5 +1,42 @@
-const FillBasket: React.FC = () => {
-  return <div className="items_block"></div>;
+import { ICart } from "../../../client_Api/interfaces";
+
+type CartType = {
+  cart: ICart;
+};
+
+const FillBasket: React.FC<CartType> = (props) => {
+  return (
+    <div className="items_block">
+      <div className="total-cost">
+        <span>Total Cost</span>
+        <span>
+          <span>{props.cart.totalPrice.centAmount / 100} </span>
+          <span>{props.cart.totalPrice.currencyCode}</span>
+        </span>
+      </div>
+      <div className="items">
+        {props.cart.lineItems.map((item) => (
+          <div className="item" key={item.id}>
+            <img src={item.variant.images[0].url} alt={item.name["en-US"]} height="100"></img>
+            <span>{item.name["en-US"]}</span>
+            <span>
+              <span>{item.price.value.centAmount / 100} </span>
+              <span>{item.price.value.currencyCode}</span>
+            </span>
+            <span>{item.quantity}</span>
+            <span>
+              <span>{item.totalPrice.centAmount / 100} </span>
+              <span>{item.totalPrice.currencyCode}</span>
+            </span>
+            <button className="delete-item">Delete</button>
+          </div>
+        ))}
+      </div>
+      <div className="delete__block">
+        <button className="delete-all">Delete All</button>
+      </div>
+    </div>
+  );
 };
 
 export default FillBasket;
