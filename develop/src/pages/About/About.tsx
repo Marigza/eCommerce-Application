f97c15e8aaca2444446ae01fb2e1d1e7f27f025b
bizhluzdy <murdrum22@gmail.com>
2023-09-13 11:48:25 +0200

    init: start developing about us page

---
+++ develop/src/pages/About/About.tsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet";
+import "./About.scss";
+
+const About: React.FC = () => {
+  return (
+    <>
+      <Helmet>
+        <title>JustStore - About us</title>
+      </Helmet>
+    </>
+  );
+};
+
+export default About;
