0888c17a3e61b321828dd8b0669f5c6132dc9ce9
mikhaelHan <hancharuk270185@mail.ru>
2023-09-14 15:51:35 +0300

    fix: fix chenge visible in cart

--- develop/src/components/BasketContent/BasketContent.tsx
+++ develop/src/components/BasketContent/BasketContent.tsx
@@ -8,21 +8,26 @@ import "./BasketContent.scss";
 
 const BasketContent: React.FC = () => {
   const [Cart, setCart] = useState<ICart>();
+  const [count, setCount] = useState(0);
+
+  const fetchData = async () => {
+    const response = await getActiveCart();
+    if (!response) return null;
+    setCart(response);
+  };
+
+  const countIncrease = () => {
+    setCount(count + 1);
+  };
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await getActiveCart();
-      if (!response) return null;
-      setCart(response);
-    }
-
-    fetchData();
-  }, []);
+    fetchData().then(() => {});
+  }, [count]);
 
   return (
     <main className="BasketContent">
       <h2 className="basket_header">Basket</h2>
-      {Cart?.lineItems.length && <FillBasket cart={Cart} />}
+      {Cart?.lineItems.length && <FillBasket cart={Cart} count={countIncrease} />}
       {!Cart?.lineItems.length && <EmptyBasket />}
     </main>
   );
