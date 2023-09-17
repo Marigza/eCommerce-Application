10fb6cb0e77862f409c987d01499fed5f838302c
Marigza <marigola@mail.ru>
2023-09-17 19:32:04 +0300

    fix: remove link from add-to-cart button

--- develop/src/components/CatalogProduct/CatalogProduct.tsx
+++ develop/src/components/CatalogProduct/CatalogProduct.tsx
@@ -50,72 +50,78 @@ const CatalogProduct: React.FC<ProductType> = (props) => {
   };
 
   return (
-    <Link to={toPath} className="product" onClick={() => writeIdProduct(props.product.id)}>
-      <div
-        className="product-image"
-        style={{ backgroundImage: `url(${props.product.masterVariant.images[0].url})` }}
-      ></div>
-      <div className="product-description">
-        <h3>{props.product.name["en-US"]}</h3>
-        <Rating name="size-small" defaultValue={5} readOnly style={{ fontSize: "2vh" }} />
-        <div className="product-description__attributes">
-          {props.product.masterVariant.attributes.map((attribute, index) => (
-            <p key={index}>
-              {attribute.name}: {attribute.value}
-            </p>
-          ))}
-        </div>
-        <div>
-          <div className="product-price">
-            {props.product.masterVariant.prices[0].discounted && (
-              <div className="product-price__old">
-                <p className="old-price">
-                  {props.product.masterVariant.prices[0].discounted?.value.centAmount
-                    ? props.product.masterVariant.prices[0].value.centAmount / 100
-                    : ""}
-                  .00$
-                </p>
-                <p className="discount-percent">
-                  -
-                  {props.product.masterVariant.prices[0].discounted?.value.centAmount
-                    ? ((props.product.masterVariant.prices[0].value.centAmount -
-                        props.product.masterVariant.prices[0].discounted.value.centAmount) /
-                        props.product.masterVariant.prices[0].value.centAmount) *
-                      100
-                    : ""}
-                  %
-                </p>
-              </div>
-            )}
-            <p
-              className={
-                props.product.masterVariant.prices[0].discounted ? "discont-price" : "normal-price"
-              }
-            >
-              {props.product.masterVariant.prices[0].discounted?.value.centAmount
-                ? props.product.masterVariant.prices[0].discounted.value.centAmount / 100 + `0`
-                : props.product.masterVariant.prices[0].value.centAmount / 100 + `.00`}
-              $
-            </p>
-          </div>
-        </div>
-        <p className="shipping">
-          free shipping <LocalShippingIcon style={{ fontSize: "2.5vh" }} />
-        </p>
-      </div>
-      {!IsInCart(props.product.id) ? (
+    <div className="product__wpapper">
+      <Link to={toPath} className="product" onClick={() => writeIdProduct(props.product.id)}>
         <div
-          className="buy-now"
-          onClick={() => {
-            addProductToCart(props.product.id);
-          }}
-        >
-          <AddShoppingCartOutlinedIcon style={{ fontSize: "4vh" }} />
+          className="product-image"
+          style={{ backgroundImage: `url(${props.product.masterVariant.images[0].url})` }}
+        ></div>
+        <div className="product-description">
+          <h3>{props.product.name["en-US"]}</h3>
+          <Rating name="size-small" defaultValue={5} readOnly style={{ fontSize: "2vh" }} />
+          <div className="product-description__attributes">
+            {props.product.masterVariant.attributes.map((attribute, index) => (
+              <p key={index}>
+                {attribute.name}: {attribute.value}
+              </p>
+            ))}
+          </div>
+          <div>
+            <div className="product-price">
+              {props.product.masterVariant.prices[0].discounted && (
+                <div className="product-price__old">
+                  <p className="old-price">
+                    {props.product.masterVariant.prices[0].discounted?.value.centAmount
+                      ? props.product.masterVariant.prices[0].value.centAmount / 100
+                      : ""}
+                    .00$
+                  </p>
+                  <p className="discount-percent">
+                    -
+                    {props.product.masterVariant.prices[0].discounted?.value.centAmount
+                      ? ((props.product.masterVariant.prices[0].value.centAmount -
+                          props.product.masterVariant.prices[0].discounted.value.centAmount) /
+                          props.product.masterVariant.prices[0].value.centAmount) *
+                        100
+                      : ""}
+                    %
+                  </p>
+                </div>
+              )}
+              <p
+                className={
+                  props.product.masterVariant.prices[0].discounted
+                    ? "discont-price"
+                    : "normal-price"
+                }
+              >
+                {props.product.masterVariant.prices[0].discounted?.value.centAmount
+                  ? props.product.masterVariant.prices[0].discounted.value.centAmount / 100 + `0`
+                  : props.product.masterVariant.prices[0].value.centAmount / 100 + `.00`}
+                $
+              </p>
+            </div>
+          </div>
+          <p className="shipping">
+            free shipping <LocalShippingIcon style={{ fontSize: "2.5vh" }} />
+          </p>
         </div>
-      ) : (
-        <div className="alredy-in-cart">already in cart</div>
-      )}
-    </Link>
+      </Link>
+      <div>
+        {!IsInCart(props.product.id) ? (
+          <div
+            className="buy-now"
+            onClick={() => {
+              addProductToCart(props.product.id);
+            }}
+          >
+            <AddShoppingCartOutlinedIcon style={{ fontSize: "4vh" }} />
+          </div>
+        ) : (
+          <div className="alredy-in-cart">already in cart</div>
+        )}
+      </div>
+    </div>
   );
 };
 
